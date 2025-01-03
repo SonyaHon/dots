f158be520fa05c6a142378006cbf712d117c071c
sonyahon <volkov030@gmail.com>
2025-01-03 13:27:58 +0400

    :package: add additional dependencies

--- install.sh
+++ install.sh
@@ -2,6 +2,17 @@
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 ## Dependencies
-brew install rg fzf zoxide
+brew install stow tmux rg fzf zoxide
 brew install --cask raycast
 brew install --cask ghostty
+
+## TPM
+if [ -d "~/.tmux/plugins/tpm" ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+## Emacs
+if ! type "emacs" > /dev/null; then
+    brew tap d12frosted/emacs-plus
+    brew install emacs-plus@31 --with-ctags --with-imagemagick --with-native-comp
+fi
